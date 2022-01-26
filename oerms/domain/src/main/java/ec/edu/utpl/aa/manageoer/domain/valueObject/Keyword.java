e3f3b3a014b2bc842c873fd29ab915a14b496829
CarlosCastillo10 <carloscastillo090916@gmail.com>
2022-01-26 14:43:19 -0500

    :sparkles: Adding Keyword value object

---
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueObject/Keyword.java
@@ -0,0 +1,11 @@
+package ec.edu.utpl.aa.manageoer.domain.valueObject;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Keyword {
+    private String word;
+}
