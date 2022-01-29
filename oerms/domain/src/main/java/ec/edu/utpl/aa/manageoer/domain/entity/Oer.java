dc95fc0b8393d557c710dff969561c35c5f6516f
CarlosCastillo10 <carloscastillo090916@gmail.com>
2022-01-29 12:39:56 -0500

    :sparkles: Adding oer entity

---
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/entity/Oer.java
@@ -0,0 +1,24 @@
+package ec.edu.utpl.aa.manageoer.domain.entity;
+
+import ec.edu.utpl.aa.manageoer.domain.valueObject.*;
+import lombok.*;
+import java.sql.Date;
+import java.util.List;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class Oer {
+    private String title;
+    private String description;
+    private Date creation_date;
+    private Date update_date;
+    private List<Author> authors;
+    private Category category;
+    private Collaborator collaborator;
+    private List<File> files;
+    private List<Keyword> keywords;
+    private License license;
+    private Platform platform;
+    private State state;
+}
