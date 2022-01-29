1aa085e722e81c2814c800e1df53f2af29a29e3a
CarlosCastillo10 <carloscastillo090916@gmail.com>
2022-01-29 13:32:50 -0500

    :wrench: Adding domain module info

---
+++ oerms/domain/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module domain {
+    exports ec.edu.utpl.aa.manageoer.domain.entity;
+    exports ec.edu.utpl.aa.manageoer.domain.valueObject;
+    requires static lombok;
+    requires static java.sql;
+}
