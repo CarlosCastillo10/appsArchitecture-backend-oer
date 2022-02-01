a38f8cd199b13b7109b11a3438d43139db0bfecb
CarlosCastillo10 <carloscastillo090916@gmail.com>
2022-02-01 10:13:04 -0500

    :sparkles: Adding use case package

---
+++ oerms/application/src/main/java/ec/edu/utpl/aa/application/usecases/PersonalOerManagementUseCase.java
@@ -0,0 +1,17 @@
+package ec.edu.utpl.aa.application.usecases;
+
+import ec.edu.utpl.aa.manageoer.domain.entity.Oer;
+import ec.edu.utpl.aa.manageoer.domain.valueobjects.*;
+
+import java.util.Date;
+import java.util.List;
+
+public interface PersonalOerManagementUseCase {
+    Oer createOer(String title, String description, Date creation_date, Date update_date, List<Author> authors,
+                  Category category, Collaborator collaborator, List<File> files, List<Keyword> keywords, License license,
+                  Platform platform, State state);
+
+    Oer retrieveOer();
+    Oer updateOer();
+    Oer deleteOer();
+}
