6565ca5a8837b5d48836b2770cf41d85277f92de
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 12:39:14 -0500

    :tada: initial Category value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Category.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Category.java
@@ -2,22 +2,3 @@ package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
 import lombok.*;
 
-@AllArgsConstructor
-@Getter
-@ToString
-public enum Category {
-    LABORATORY("Laboratorio/Práctica"),
-    DATASET("Dataset"),
-    FULL_COURSE("Curso completo"),
-    TASK("Tarea"),
-    MASTER_CLASS("Clase magistral"),
-    REPOSITORY("Repositorio"),
-    NOTES("Notas/Apuntes"),
-    LESSON("Lección"),
-    MODULE("Módulo"),
-    READING("Lectura"),
-    SYLLABUS("Programa de estudios"),
-    TEXT_BOOK("Libro de texto");
-
-    private final String description;
-}
