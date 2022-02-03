6afe304997ff140367aab2654487762ce3b25c8d
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 13:07:19 -0500

    :tada: initial File value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/File.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/File.java
@@ -1,12 +1,4 @@
 package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 import lombok.*;
 
-@Builder
-@AllArgsConstructor
-@Getter
-@ToString
-@EqualsAndHashCode
-public class File {
-    private String location;
-    private DistributionFormat distributionFormat;
-}
+
