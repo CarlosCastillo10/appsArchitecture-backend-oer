0428dea12f9908381f80e49ea63d309be290e511
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 12:51:01 -0500

    :hammer: update DistributionFormatFormat value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/DistributionFormat.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/DistributionFormat.java
@@ -2,3 +2,12 @@ package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
 import lombok.*;
 
+@Builder
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class DistributionFormat {
+    private String distribution;
+    private Extension extension;
+}
