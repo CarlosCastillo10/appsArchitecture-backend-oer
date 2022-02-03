4738beec2029fcb92f27f6d498bbf57dad64ea7b
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 13:12:50 -0500

    :hammer: update License value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/License.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/License.java
@@ -1,3 +1,15 @@
 package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
 import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class License {
+    private String type;
+    private String acronym;
+    private String description;
+    private String icon;
+}
