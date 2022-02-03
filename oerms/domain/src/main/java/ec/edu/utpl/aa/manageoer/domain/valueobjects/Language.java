f7efee044b6d88a13cdb3c119971db1e9ff71ade
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 13:11:22 -0500

    :hammer: update Languaje value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Language.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Language.java
@@ -2,4 +2,19 @@ package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
 import lombok.*;
 
+@AllArgsConstructor
+@Getter
+@ToString
+public enum Language {
+    CHINESE("Chino"),
+    SPANISH("Español"),
+    ENGLISH("Inglés"),
+    PORTUGUESE("Portugués"),
+    RUSSIAN("Ruso"),
+    JAPANESE("Japonés"),
+    FRENCH("Francés"),
+    GERMAN("Alemán"),
+    ITALIAN("Italiano");
 
+    private final String name;
+}
