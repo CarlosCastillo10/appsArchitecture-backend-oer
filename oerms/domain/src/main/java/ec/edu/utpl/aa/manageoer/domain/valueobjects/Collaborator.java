2766236ac0e913c87c3db689bd321e1061a6f687
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 12:47:47 -0500

    :hammer: update Collaborator value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Collaborator.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Collaborator.java
@@ -2,4 +2,15 @@ package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
 import lombok.*;
 
-
+@Builder
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Collaborator {
+    private String name;
+    private String lastname;
+    private String email;
+    private String password;
+    private String avatar;
+}
