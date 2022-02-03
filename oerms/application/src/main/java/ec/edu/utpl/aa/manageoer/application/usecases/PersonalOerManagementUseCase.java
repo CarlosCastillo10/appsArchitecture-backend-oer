76291f56285ff68f19b9fe76805ca3b268e7b4be
lderazo1 <luiyi.darmijos@gmail.com>
2022-02-03 15:19:37 -0500

    :tada: Initial Use Case Interface

--- oerms/application/src/main/java/ec/edu/utpl/aa/manageoer/application/usecases/PersonalOerManagementUseCase.java
+++ oerms/application/src/main/java/ec/edu/utpl/aa/manageoer/application/usecases/PersonalOerManagementUseCase.java
@@ -6,13 +6,5 @@ import java.sql.Date;
 import java.util.List;
 
 public interface PersonalOerManagementUseCase {
-    Oer createOer(String title, String description, Date creation_date, Date update_date, List<Author> authors,
-                  Category category, Collaborator collaborator, List<File> files, List<Keyword> keywords, License license,
-                  Platform platform, State state);
-
-    Oer retrieveOer(int id);
-    Oer updateOer(int id, String title, String description, Date creation_date, Date update_date, List<Author> authors,
-                  Category category, Collaborator collaborator, List<File> files, List<Keyword> keywords, License license,
-                  Platform platform, State state);
-    void deleteOer(int id);
+    
 }
