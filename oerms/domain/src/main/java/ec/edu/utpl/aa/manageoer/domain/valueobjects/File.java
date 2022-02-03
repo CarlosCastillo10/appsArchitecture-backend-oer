70262acb1a85091edc369b59e0859369e6f98e7b
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 13:07:49 -0500

    :hammer: update File value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/File.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/File.java
@@ -1,4 +1,12 @@
 package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 import lombok.*;
 
-
+@Builder
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class File {
+    private String location;
+    private DistributionFormat distributionFormat;
+}
