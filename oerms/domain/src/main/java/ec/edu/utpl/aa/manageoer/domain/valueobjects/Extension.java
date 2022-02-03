1079c8403677d7881a815bba00355528ca6a055d
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 12:55:24 -0500

    :tada: initial Extension value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Extension.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Extension.java
@@ -1,9 +1,2 @@
 package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
-public enum Extension {
-    MP3, WAV, // Audio
-    ZIP, RAR, // Download documents
-    JPG, GIF, PNG, // Images
-    DOC, DOCX, XLS, XLSX, PPTX, TXT, HTML, PDF, // Text, HTML
-    AVI, MPEG, MP4 // Video
-}
