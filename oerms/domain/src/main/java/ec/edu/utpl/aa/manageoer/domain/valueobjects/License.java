147294cb9ada84832454a375b2ae1350fcb4879a
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 13:12:11 -0500

    :tada: initial License value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/License.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/License.java
@@ -1,15 +1,3 @@
 package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
 import lombok.*;
-
-@Builder
-@AllArgsConstructor
-@Getter
-@ToString
-@EqualsAndHashCode
-public class License {
-    private String type;
-    private String acronym;
-    private String description;
-    private String icon;
-}
