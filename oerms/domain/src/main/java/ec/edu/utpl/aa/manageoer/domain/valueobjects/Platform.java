cf009c50238cef4e6254a8dc018f005a985d2b24
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 13:14:03 -0500

    :hammer: update Plarform value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Platform.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Platform.java
@@ -2,3 +2,12 @@ package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
 import lombok.*;
 
+@Builder
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Platform {
+    private String name;
+    private String url;
+}
