1399f673ab2f8dcb368bc7ea872d628144939139
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 13:10:10 -0500

    :tada: initial Languaje value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Language.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Language.java
@@ -2,19 +2,4 @@ package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
 import lombok.*;
 
-@AllArgsConstructor
-@Getter
-@ToString
-public enum Language {
-    CHINESE("Chino"),
-    SPANISH("Español"),
-    ENGLISH("Inglés"),
-    PORTUGUESE("Portugués"),
-    RUSSIAN("Ruso"),
-    JAPANESE("Japonés"),
-    FRENCH("Francés"),
-    GERMAN("Alemán"),
-    ITALIAN("Italiano");
 
-    private final String name;
-}
