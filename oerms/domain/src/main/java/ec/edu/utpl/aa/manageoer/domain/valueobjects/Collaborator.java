ab0bfb270ea7db6b5be7de50c7dba5ab7a7c9363
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 12:44:02 -0500

    :tada: initial Collaborator value object

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Collaborator.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/valueobjects/Collaborator.java
@@ -2,15 +2,4 @@ package ec.edu.utpl.aa.manageoer.domain.valueobjects;
 
 import lombok.*;
 
-@Builder
-@AllArgsConstructor
-@Getter
-@ToString
-@EqualsAndHashCode
-public class Collaborator {
-    private String name;
-    private String lastname;
-    private String email;
-    private String password;
-    private String avatar;
-}
+
