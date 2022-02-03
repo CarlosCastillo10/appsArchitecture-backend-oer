c331d669155a5ebaedbceb08f9e228746a4474e7
Luisdiazarmijos <lucho2911diaz@gmail.com>
2022-02-03 13:17:51 -0500

    :hammer: update Oer entity

--- oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/entity/Oer.java
+++ oerms/domain/src/main/java/ec/edu/utpl/aa/manageoer/domain/entity/Oer.java
@@ -5,3 +5,20 @@ import lombok.*;
 import java.sql.Date;
 import java.util.List;
 
+@Builder
+@Getter
+@AllArgsConstructor
+public class Oer {
+    private String title;
+    private String description;
+    private Date creation_date;
+    private Date update_date;
+    private List<Author> authors;
+    private Category category;
+    private Collaborator collaborator;
+    private List<File> files;
+    private List<Keyword> keywords;
+    private License license;
+    private Platform platform;
+    private State state;
+}
